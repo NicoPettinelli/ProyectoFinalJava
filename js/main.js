c8054d7cb869215226f107cff8e761785e021285
NicoPettinelli <niadpe@gmail.com>
2024-06-08 21:48:20 -0300

    proyectoFinal

---
+++ js/main.js
@@ -0,0 +1,51 @@
+let gastos = [];
+
+function cargarGastos() {
+    const gastosGuardados = localStorage.getItem('gastos');
+    if (gastosGuardados) {
+        gastos = JSON.parse(gastosGuardados);
+        mostrarGastos();
+        calcularTotal();
+    }
+}
+
+
+function agregarGasto() {
+    const nombre = document.getElementById('nombre').value;
+    const monto = parseFloat(document.getElementById('monto').value);
+
+    if (nombre && !isNaN(monto)) {
+        const gasto = {
+            nombre: nombre,
+            monto: monto
+        };
+
+        gastos.push(gasto);
+
+        localStorage.setItem('gastos', JSON.stringify(gastos));
+
+        document.getElementById('nombre').value = '';
+        document.getElementById('monto').value = '';
+
+        mostrarGastos();
+        calcularTotal();
+    } else {
+        alert('Por favor, ingrese un nombre y un monto válido.');
+    }
+}
+
+function mostrarGastos() {
+    const listaGastos = document.getElementById('listaGastos');
+    listaGastos.innerHTML = '';
+
+    gastos.forEach(gasto => {
+        const li = document.createElement('li');
+        li.textContent = `${gasto.nombre}: $${gasto.monto}`;
+        listaGastos.appendChild(li);
+    });
+}
+
+function calcularTotal() {
+    const total = gastos.reduce((sum, gasto) => sum + gasto.monto, 0);
+    document.getElementById('totalGastos').textContent = `$${total}`;
+}
